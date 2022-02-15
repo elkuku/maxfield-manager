c805fff3653364798164488f191c6aba3e3463b4
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-15 10:28:42 -0500

    Show maxfields on homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,25 +3,21 @@
 namespace App\Controller;
 
 use Elkuku\MaxfieldParser\MaxfieldParser;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class DefaultController extends BaseController
 {
     #[Route('/', name: 'default', methods: ['GET'])]
     public function index(
         string $projectDir,
     ): Response {
-        $parser = new MaxfieldParser();
+        $maxfields = $this->getUser()?->getMaxfields();
         return $this->render(
             'default/index.html.twig',
             [
-                'controller_name' => 'DefaultController',
-                'php_version'     => PHP_VERSION,
-                'symfony_version' => Kernel::VERSION,
-                'project_dir'     => $projectDir,
+                'maxfields' => $this->getUser()?->getMaxfields(),
             ]
         );
     }
