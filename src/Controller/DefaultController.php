27149e278d072f4d6877bd770ea28ac10e03ff7b
Nikolai Plath <der.el.kuku@gmail.com>
2022-02-17 10:25:34 -0500

    Cleanup

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,13 @@
 
 namespace App\Controller;
 
-use Elkuku\MaxfieldParser\MaxfieldParser;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends BaseController
 {
     #[Route('/', name: 'default', methods: ['GET'])]
-    public function index(
-        string $projectDir,
-    ): Response {
-        $maxfields = $this->getUser()?->getMaxfields();
+    public function index(): Response {
         return $this->render(
             'default/index.html.twig',
             [
